994ff17eba7b9116716099a24dcb3fa6e523b11a
Yousef <Yalhash@users.noreply.github.com>
2018-10-20 19:08:25 -0300

    Update predictions.py

--- predictions.py
+++ predictions.py
@@ -1,42 +1,44 @@
-import autocomplete as ac
+import autocomplete
 from autocomplete import models
+
+DEFAULTPREDICTION = ['the', 'hello', 'oh yeah', 'wow', '~']
+lastWord = ''
+currentWord = ''
 
 #import 'file'
 def startup():
-    ac.load()
+    autocomplete.load()
+    return DEFAULTPREDICTION
 
 def makePrediction(character):
-    wordList = ac.predict(currentString, currentWord)
+    global lastWord
+    global currentWord
+    #update lastWord, currentWord, lastString
+    if(character == ' '):
+        lastWord = currentWord
+        currentWord = ''
+    else:
+        currentWord += character
+    wordList = pasturizeList(autocomplete.predict(lastWord, currentWord))
+    
     return wordList
 
-def flushPredictions(flushedString):
+def flushPredictions():
+    global lastString
+    global lastWord
+    global currentWord
     #restart prediction list, add the sentance to the big.txt file
-
+    
     #add to predictive model
-    bigtxtpath = os.path.join(os.path.dirname(__file__), 'big.txt')
-    with open(bigtxtpath, 'a') as bigtxtfile:
-        bigtxtfile.write(flushedString)
-
-def pasturizeList(wordList):
-    return sorted(wordList, key=lambda tup: tup[1])
-
-    return newList
-
-def debugStream():
-    lastString = ''
+    if(len(lastString) > 30):
+        ac.models.train_models(lastString)
+        lastString = ''
+    else:
+        lastString = '\n'        
+    #resets things
     lastWord = ''
     currentWord = ''
-    while(True):
-        currentWord += input('give char')
-        if(currentWord[-1] == ' '):
-            lastString += lastWord
-            lastWord = currentWord
-            currentWord = ''
-        else:
-            wordList = pasturizeList(ac.predict(lastWord, currentWord))
+    return DEFAULTPREDICTION
 
-
-#cannot pass space character into function as all things must be a word!
-if __name__ == '__main__':
-    startup()
-    debugStream()
+def pasturizeList(wordList):
+    return [i[0] for i in sorted(wordList, key=lambda tup: tup[1]) ]
