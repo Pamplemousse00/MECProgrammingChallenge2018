d17e3330147cd2ed370c3c2821bf94d33831e7d9
Pamplemousse00 <31556084+Pamplemousse00@users.noreply.github.com>
2018-10-20 15:29:53 -0400

    webget and some other stuff that i dont know

--- webGet.py
+++ webGet.py
@@ -1,4 +1,15 @@
 from flask import Flask, render_template, jsonify
+import predictions
+from predictions import *
+
+predictions.makePredictions('f');
+
+
+word1 = "test1"
+word2 = "test2"
+word3 = "test3"
+
+dictSend = {'word1': word1, 'word2': word2, 'word3': word3}
 
 app = Flask(__name__)
 
@@ -7,7 +18,9 @@ app = Flask(__name__)
 def index():
 	return render_template('./test.html')
 
-
+@app.route("/getData", methods=['GET'])
+def getData():
+	return jsonify(dictSend)
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0')
