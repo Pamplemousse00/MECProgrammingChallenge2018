54ef3e9974000def6b01a42767e5c9c934115bd1
Yousef <Yalhash@users.noreply.github.com>
2018-10-20 15:49:54 -0300

    Update predictions.py

--- predictions.py
+++ predictions.py
@@ -1,7 +1,20 @@
+import autocomplete as ac
 from autocomplete import models
-import 'file'
+
+
+#import 'file'
+def startup():
+    ac.load()
 
 def makePrediction(character):
+    wordList = ac.predict(currentString, currentWord)
+    wordList.sort()
+    return wordList
 
-def flushPredictions():
-    #
+def flushPredictions(flushedString):
+    #restart prediction list, add the sentance to the big.txt file
+
+    #add to predictive model
+    bigtxtpath = os.path.join(os.path.dirname(__file__), 'big.txt')
+    with open(bigtxtpath, 'a') as bigtxtfile:
+        bigtxtfile.write(flushedString)
